3420c622b71086c05bbbd3f1eb8ce6f30f4701d8
themunish100 <85390505+themunish100@users.noreply.github.com>
2021-06-06 19:23:12 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(){
+
+  return (<header>
+    <h1>
+     WELCOME TO THIS PAGE DESIGNED BY MUNISH
+      </h1>
+  </header> );
+
+}
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note(){
+
+  return (
+    <div className="note">
+      <h1>
+Javascript and React.js Bootcamp Project
+
+      </h1>
+      <p> I have made this project as this was given to us by Shaurya Sir as the assignment for Javascript and Reactjs Bootcamp. </p>
+       </div>
+  )
+}
+export default Note;
